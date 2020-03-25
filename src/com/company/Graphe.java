996467351d42b3a9c339dadf29659ae210343417
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2020-03-25 09:33:50 +0100

    determinatin circuit + rang

--- src/com/company/Graphe.java
+++ src/com/company/Graphe.java
@@ -11,6 +11,7 @@ public class Graphe {
     private ArrayList<Transition> listTransition;
     private int[][] matriceAdjacence;
     private String[][] matriceValeur;
+    private int[] rang;
 
     public Graphe(int _num_file){
         this.listTransition = new ArrayList<Transition>(); //initialisation de la liste de transition
@@ -21,15 +22,22 @@ public class Graphe {
         //initialisation des matrices
         this.matriceValeur = new String[nb_sommet][nb_sommet];
         this.matriceAdjacence = new int[nb_sommet][nb_sommet];
+        this.rang = new int[nb_sommet];
         for (int i = 0; i < nb_sommet; i++) {
+            rang[i] = -1;
             for (int j = 0; j < nb_sommet; j++) {
                 matriceAdjacence[i][j] = 0;
-                matriceValeur[i][j] = "X";
+                matriceValeur[i][j] = "*";
             }
         }
-        createMatrice(); //remplissage des matrices
+        fillMatrice(); //remplissage des matrices
+
+        detectCircuit();
     }
 
+    /**
+     * Lit et creer la liste de transition à partir du fichier
+     */
     private void readFile(){
         try {
             Scanner scan = new Scanner(new File("Textfile/" + name_file));
@@ -44,18 +52,83 @@ public class Graphe {
         }
     }
 
+    /**
+     * creer une transition
+     */
     private Transition createTransition(String line){
         String[] array = line.split(" ");
         Transition transition = new Transition(Integer.parseInt(array[0]), Integer.parseInt(array[1]), Integer.parseInt(array[2]));
         return transition;
     }
 
-    private void createMatrice(){
+    /**
+     * remplis les matrices d'ajacence et valeurs à partir de la liste de transition
+     */
+    private void fillMatrice(){
         for (int i = 0; i < listTransition.size(); i++) {
             matriceAdjacence[listTransition.get(i).getInit()][listTransition.get(i).getFin()] = 1;
             matriceValeur[listTransition.get(i).getInit()][listTransition.get(i).getFin()] = String.valueOf(listTransition.get(i).getArc());
         }
     }
+
+    private void detectCircuit() {
+        //initialisation
+        int[][] matDetection = matriceAdjacence;
+        ArrayList<Integer> done = new ArrayList<Integer>();
+        ArrayList<Integer> todo = new ArrayList<Integer>();
+        for (int i = 0; i < nb_sommet; i++) {
+            todo.add(i);
+        }
+
+        //rang
+        int rg = 0;
+        do{
+            //un tour
+            int tempo = 0;
+            //detection predecesseur ou non
+            for (int i = 0; i < todo.size(); i++) {
+                for (int j = 0; j < nb_sommet; j++) {
+                    if (matDetection[j][todo.get(i)] == 1) { // [j][i] car parcourt en colonne
+                        tempo++;
+                    }
+                }
+                if (tempo == 0) {
+                    done.add(todo.get(i));
+                    rang[todo.get(i)] = rg;
+                }
+                tempo = 0;
+            }
+            //passage à -1 des elements
+            for (int i = 0; i < done.size(); i++) {
+                for (int j = 0; j < nb_sommet; j++) {
+                    matDetection[j][done.get(i)] = -1; //passage à -1 en colonne
+                    matDetection[done.get(i)][j] = -1; //passage à -1 en ligne
+                }
+            }
+            //suppression dans tab_todo
+            for (int i = 0; i < done.size() ; i++) {
+                todo.remove(done.get(i));
+            }
+
+
+            System.out.println("k= " + rg + "\n" + todo);
+            System.out.println(done);
+            rg++;
+        }while ((rg < nb_sommet) && (!todo.isEmpty()));
+
+
+        printMatrice(matDetection);
+    }
+
+    private void printMatrice(int[][] arrayList){
+        for (int i = 0; i < nb_sommet; i++) {
+            for (int j = 0; j < nb_sommet; j++) {
+                System.out.print(arrayList[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+    /*Getter setter*/
 
     public int getNum_file() {
         return num_file;
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,22 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Graphe a = new Graphe(1);
+        Graphe a = new Graphe(2);
+
+//        ArrayList<Integer> done = new ArrayList<Integer>();
+//        ArrayList<Integer> todo = new ArrayList<Integer>();
+//        for (int i = 0; i < 10; i++) {
+//            todo.add(i);
+//            done.add(i*2);
+//        }
+//        System.out.println(todo);
+//        System.out.println(done);
+//        System.out.println(todo);
     }
 
 
